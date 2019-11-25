506542f3ad18eef77751c999e8fcf642bae021d8
trupti-gotephode <truptigotephode@gmail.com>
2019-11-25 16:05:36 +0530

    Added system call program

---
+++ sys_call/syscall.c
@@ -0,0 +1,37 @@
+#include <sys/types.h>
+      #include <sys/stat.h>
+      #include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <errno.h>
+
+int main(int argc, char const *argv[])
+{
+	
+	
+	      int fd= open("syscall.txt",O_CREAT,O_RDONLY);
+          if(-1==fd)
+          {
+          	perror("UNABLE TO OPEN");
+          }
+
+
+         int wr= write(fd,"OS IMPLEMENTs SYSCALL IN KERNEL SPACE",37);
+         if(wr==-1)
+          {
+          	perror("UNABLE TO WRITE");
+          }
+
+
+           int cl= close( fd);
+           if(cl==-1)
+          {
+          	perror("UNABLE TO WRITE");
+          }
+
+
+
+
+
+return 0;
+}
